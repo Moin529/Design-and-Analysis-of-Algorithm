c94443bf56476c7f5bcb58a1bfc6bcc860f9f8e0
Mahmudur Rahman Moin <mrmoin529@gmail.com>
2025-07-02 19:19:20 +0000

    Update LCS.java

--- Lab 8/LCS.java
+++ Lab 8/LCS.java
@@ -2,8 +2,24 @@ import java.util.*;
 
 public class LCS {
 
-    // Recursive with memoization
-    public static int lcsRec(String s1, String s2, int i, int j, int[][] memo) {
+    // with recursion
+    static int lcsRecursion(String str1, String str2, int i, int j) {
+        if (i == 0 || j == 0) {
+            return 0; // Base case: one string is empty
+        }
+        if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+
+            int includeChar = 1 + lcsRecursion(str1, str2, i - 1, j - 1); // Include character from both strings
+            return includeChar; // Return the length of LCS including this character
+        } else {
+            int excludeStr1 = lcsRecursion(str1, str2, i - 1, j); // Exclude character from str1
+            int excludeStr2 = lcsRecursion(str1, str2, i, j - 1); // Exclude character from str2
+            return Math.max(excludeStr1, excludeStr2); // Take the maximum of excluding either character
+        }
+    }
+    
+    // with memoization
+    public static int lcsMem(String s1, String s2, int i, int j, int[][] memo) {
         if (i == 0 || j == 0) return 0;
         if (memo[i][j] != -1) return memo[i][j];
 
@@ -43,7 +59,7 @@ public class LCS {
             for (int j = 0; j <= s2.length(); j++)
                 memo[i][j] = -1;
 
-        System.out.println("LCS with memoization: " + lcsRec(s1, s2, s1.length(), s2.length(), memo));
+        System.out.println("LCS with memoization: " + lcsMem(s1, s2, s1.length(), s2.length(), memo));
         System.out.println("LCS with DP: " + lcsDP(s1, s2));
     }
 }
