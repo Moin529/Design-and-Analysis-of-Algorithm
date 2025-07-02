7d9a2544bfafad6c4c9c5f23d7d4c1ed2d7f5092
Mahmudur Rahman Moin <mrmoin529@gmail.com>
2025-07-03 00:57:41 +0600

    Add files via upload

---
+++ Lab 8/Fibonacci1.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Fibonacci1 {
+
+    public static int fibMemo(int n, HashMap<Integer, Integer> memo) {
+        if (n <= 1) return n;
+        if (memo.containsKey(n)) return memo.get(n);
+
+        int result = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+        memo.put(n, result);
+        return result;
+    }
+
+    public static int fibTab(int n) {
+        if (n <= 1) return n;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Give input 'n' : ");
+        int n = sc.nextInt();
+        System.out.println("fibMemo: " + fibMemo(n, new HashMap<>()));
+        System.out.println("fibTab: " + fibTab(n));
+    }
+}
